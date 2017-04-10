c7b25357efd8b966ce75e5d7700a9553c2bb05ae
randyagaleano <randyagaleano@gmail.com>
2017-04-10 11:17:14 -0400

    some style

---
+++ client/components/cocktails/cocktails.search/cocktails.search.form/cocktails.form.component.js
@@ -0,0 +1,11 @@
+const controller 			= require('./cocktails.form.controller.js');
+const template 				= require('./cocktails.form.html');
+
+const CocktailsFormComponent = {
+	controller: controller,
+	template: template
+};
+
+angular
+    .module('imbibleApp')
+    .component('cocktailsForm', CocktailsFormComponent);
---
+++ client/components/cocktails/cocktails.search/cocktails.search.form/cocktails.form.controller.js
@@ -0,0 +1,14 @@
+CocktailsFormController.$inject = ['CocktailsService', '$stateParams'];
+
+function CocktailsFormController(CocktailsService, $stateParams) {
+    const vm = this;
+
+    vm.coctails = [];
+
+    activate();
+
+
+}
+
+module.exports = CocktailsFormController;
+
