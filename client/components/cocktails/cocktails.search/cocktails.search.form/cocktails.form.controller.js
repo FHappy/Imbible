8cb4a60a0bd560c340d51906f67f1b81be46f1cc
FHappy <Hassan.Masroor@me.com>
2017-04-11 03:19:12 -0400

    syntax changes

--- client/components/cocktails/cocktails.search/cocktails.search.form/cocktails.form.controller.js
+++ client/components/cocktails/cocktails.search/cocktails.search.form/cocktails.form.controller.js
@@ -12,12 +12,10 @@ function CocktailsFormController(CocktailsService, $stateParams, $scope, SearchF
     };
 
     function search(newSearchTerm) {
-        console.log('search term is ' + newSearchTerm);
         SearchFactory.terms.push(newSearchTerm);
         var allSearchTerms = SearchFactory.terms;
         var searchUrl = '?';
         for (var i = 0; i < allSearchTerms.length; i++) {
-            console.log('search i is ' + i);
             searchUrl += 'q' + i + '=' + allSearchTerms[i];
             if (i !== allSearchTerms.length - 1) {
               searchUrl += '&';
@@ -29,9 +27,6 @@ function CocktailsFormController(CocktailsService, $stateParams, $scope, SearchF
               // update factory values, so it will change other components
                 SearchFactory.eitherResults = response.data.orCocktails;
                 SearchFactory.allResults = response.data.andCocktails;
-                console.log(SearchFactory.terms);
-                console.log(SearchFactory.eitherResults);
-                console.log(SearchFactory.allResults);
             }, function reject(response) {
                 return ({message: 'No results found.'});
             });
