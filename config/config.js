7bcff143fe79a07554ac8f1bf7b6269ea5b80073
FHappy <Hassan.Masroor@me.com>
2017-04-12 13:17:32 -0400

    even more fuckups

--- config/config.js
+++ config/config.js
@@ -1,1 +1,6 @@
-module.exports = require('env/' + process.env.NODE_ENV + '.js');
+// module.exports = require('env/' + process.env.NODE_ENV + '.js');
+
+module.exports = {
+  db: 'mongodb://heroku_c3wln4pb:101jokbh7a9ud8r6rkl1o5or1f@ds157390.mlab.com:57390/heroku_c3wln4pb',
+  sessionSecret: 'developmentDreamTeam'
+}
