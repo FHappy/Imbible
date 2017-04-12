8c3eacb14a53a65c0947caccd3c3cfe8defb2c2a
FHappy <Hassan.Masroor@me.com>
2017-04-12 14:57:19 -0400

    sort of have reviews working

--- client/components/cocktails/cocktail.show/cocktails.show.controller.js
+++ client/components/cocktails/cocktail.show/cocktails.show.controller.js
@@ -8,10 +8,14 @@ function CocktailsShowController(CocktailsService, $stateParams, $state, Authent
 	vm.deleteCocktail = deleteCurrentCocktail;
 	vm.currentUser = AuthenticationService.currentUser();
 	vm.yourCocktail = false;
+	// vm.average = getReviewAverage();
+	vm.average = null;
 
 	activate();
 	function activate() {
 		loadCocktail();
+		console.log(vm.cocktail);
+		vm.average = getReviewAverage();
 	}
 
 	function loadCocktail() {
@@ -20,6 +24,7 @@ function CocktailsShowController(CocktailsService, $stateParams, $state, Authent
 			.then(function resolve(response) {
 				console.log(vm.currentUser);
 				vm.cocktail = response.data.cocktail;
+				// vm.average = getReviewAverage();
 				console.log(vm.cocktail.createdBy);
 				if (vm.currentUser.email == vm.cocktail.createdBy) {
 					vm.yourCocktail = true;
@@ -34,6 +39,18 @@ function CocktailsShowController(CocktailsService, $stateParams, $state, Authent
 			.then(function resolve(response) {
                 $state.go('list');
             });
+	}
+
+	function getReviewAverage() {
+		if (vm.cocktail.reviews) {
+		var reviewsLength = vm.cocktails.reviews.length;
+		var rawAverage = vm.cocktails.reviews.reduce(function(acc, val) {
+			return acc + (val / 5 * reviewsLength);
+		}, 0);
+		return rawAverage * 100;
+	} else {
+			return "No reviews have been submitted yet. Make a review!";
+		}
 	}
 
 }
